8134b9cb557632d0e6d5b3ecab2590f3200349f3
Zach Upstone <zu213@bath.ac.uk>
2024-12-28 22:53:53 +0000

    Added first few rules to chess

--- ascii_chess.c
+++ ascii_chess.c
@@ -24,47 +24,48 @@
  black king 12
 */
 
-int print_board(int pieces[]){
+void setup_board(int *board){
     const int board_length = 8;
-    int board[64] = {0};
+    //int board[64] = {0};
     int i = 0;
-    int pieces_length = 32;
 
-    for(i = 0; i < pieces_length; i++){
-        //wprintf(L" %i,", pieces[i]);
-        // early return
-        if(pieces[i] < 0){
-            continue;
-        }
+    for(i = 0; i < 32; i++){
 
-        if(i < 8){
-            board[pieces[i]] = 1;
-        }else if(i == 8 || i ==15){
-            board[pieces[i]] = 2;
-        }else if(i == 9 || i ==14){
-            board[pieces[i]] = 3;
-        }else if(i == 10 || i ==13){
-            board[pieces[i]] = 4;
-        }else if(i == 11){
-            board[pieces[i]] = 5;
-        }else if(i == 12){
-            board[pieces[i]] = 6;
+        if(i == 0 || i == 7){
+            board[i] = 2;
+        }else if(i == 1 || i == 6){
+            board[i] = 3;
+        }else if(i == 2 || i == 5){
+            board[i] = 4;
+        }else if(i == 3){
+            board[i] = 5;
+        }else if(i == 4){
+            board[i] = 6;
+        }else if(i < 16){
+            board[i] = 1;
         }else if(i < 24){
-            board[pieces[i]] = 7;
+            board[i + 32] = 7;
         }else if(i == 24 || i == 31){
-            board[pieces[i]] = 8;
+            board[i + 32] = 8;
         }else if(i == 25 || i == 30){
-            board[pieces[i]] = 9;
+            board[i + 32] = 9;
         }else if(i == 26 || i == 29){
-            board[pieces[i]] = 10;
+            board[i + 32] = 10;
         }else if(i == 27){
-            board[pieces[i]] = 11;
+            board[i + 32] = 11;
         }else if(i == 28){
-            board[pieces[i]] = 12;
+            board[i + 32] = 12;
         }else{
             wprintf(L"Error");
         }
     }
+
+}
+
+int print_board(int board[]){
+    int i = 0;
+    const int board_length = 8;
+    
     wchar_t black_square = L'□';
     wchar_t white_square = L'■';
     wchar_t white_pawn = L'♟';
@@ -115,20 +116,94 @@ int print_board(int pieces[]){
             }else{
                 wprintf(L" %lc", black_square);
             }
-
         }
         wprintf(L" a \n");
 
     }
-    printf(" ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜ ");
-    printf(" ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟ ");
-    printf(" ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙ ");
-    printf(" ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖ ");
 
     fflush( stdout );
     return 0;
 }
 
+int char_to_row(char c){
+    if(c == 'A'){
+        return 0;
+    }else if(c == 'B'){
+        return 1;
+    }else if(c == 'C'){
+        return 2;
+    }else if(c == 'D'){
+        return 3;
+    }else if(c == 'E'){
+        return 4;
+    }else if(c == 'F'){
+        return 5;
+    }else if(c == 'G'){
+        return 6;
+    }else if(c == 'H'){
+        return 7;
+    }else{
+        wprintf(L"invalid character %c", c);
+        return -1;
+    }
+}
+
+int check_path(int start, int end){
+
+}
+
+int valid_move_for_piece(int piece, int start, int end){
+    if(end < 0 || end > 63 || end == start){
+        return 0;
+    }
+    if(piece == 1){
+        if((((end - start) > 6 && (end - start) < 9) || (start < 16 && (end - start) == 16)) && (end - start) > 0){
+            return 1;
+        }
+    }else if(piece == 2){
+        int row_start = start - (start % 8);
+        if(end % 8 == start % 8 || (end - row_start < 8 && end - row_start > 0)){
+            return 1;
+        }
+    }else if(piece == 3){
+        int row_start = start - (start % 8);
+        if(end % 8 == start % 8 || (end - row_start < 8 && end - row_start > 0)){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void move_piece(int *board, wchar_t move[], int *next_move){
+    int move_row = char_to_row(move[0]);
+    int move_column = (int)move[1] - '0' - 1;
+    int result_row = char_to_row(move[4]);
+    int result_column = (int)move[5]- '0' - 1;
+    int piece;
+    int pinned = 0;
+    int valid_move = 0;
+
+    if(!pinned){
+        piece = board[move_row * 8 + move_column];
+        if(piece == 0 || (*next_move == 0 && piece > 7) || (*next_move == 1 && piece < 8)){
+            return;
+        }
+        if(valid_move_for_piece(piece, move_row * 8 + move_column, result_row * 8 + result_column)){
+            if(piece == 3 || piece == 9 || check_path(move_row * 8 + move_column, result_row * 8 + result_column)){
+                valid_move = 1;
+            }
+        }
+        
+        if(valid_move){
+            board[move_row * 8 + move_column] = 0;
+            wprintf(L"%i ccc %i DDDD",piece, move_column);
+            board[result_row * 8 + result_column] = piece;
+            *next_move = *next_move * -1 + 1;
+            wprintf(L"%i ccc %i DDDD",piece, board[result_row * 8 + result_column]);
+        }
+    }
+
+}
 /*
  Piece mappping for list storage
  white pawns 0-7
@@ -149,32 +224,34 @@ int print_board(int pieces[]){
 int main(){
     _setmode(_fileno(stdout), 0x00020000);
     wchar_t move[50];
-
-    int pieces[32] = {
-     8, 9, 10, 11, 12, 13, 14, 15,
-     0, 1, 2, 3, 4, 5 ,6 , 7,
-     48, 49, 50, 51, 52, 53, 54, 55,
-     56, 57, 58, 59, 60, 61, 62, 63};
+    int board[64] = {0};
+    int next_move;
+    int started = 0;
+    setup_board(board);
 
     wprintf(L"WELCOME TO CHESS, TYPE 'start' TO BEGIN OR 'exit' TO END \n");
     const wchar_t *target = L"exit";
     while(1){
         if (wscanf(L"%99[^\n]", move) == 1){
-            if (wcscmp(move, L"start") == 0)
+            while (getwchar() != '\n');
+
+            if (wcscmp(move, L"start") == 0 && !started)
             {
-                print_board(pieces);
-                while(1){
-                    if (wscanf(L"%99[^\n]", move) == 1){
-                        wprintf(L"\033[2J\033[H");
-                        wprintf(L"%ls", move);
-                        print_board(pieces);
-                    }
-                }
-            }else if(wcscmp(move,target) == 0){
+                started = 1;
+                next_move = 0;
+                print_board(board);
+            }else if(started){
+                move_piece(board, move, &next_move);
+                wprintf(L"%ls", move);
+                print_board(board);
+                wprintf(L"%i",board[0]);
+            }else if(wcscmp(move,L"end") == 0){
+                int started = 0;
+                setup_board(board);
+            }else if(wcscmp(move,L"exit") == 0){
                 break;
             }
             wprintf(L"%s\n",move);
-            
         }
     }
     
